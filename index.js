699a9a227f17794f39efefb78545d4bf413f8fcc
norlin <myxaxaxa@gmail.com>
2013-11-24 14:52:26 +0400

    mtgox2prowl

---
+++ index.js
@@ -0,0 +1,71 @@
+var sys = require('util'),
+	gox = require('goxstream'),
+	Prowl = require('node-prowl'),
+	result,
+	last,
+	min,
+	remain,
+	timeout;
+
+result = gox.createStream();
+
+var json = '';
+
+result.on('data', output);
+
+function output (data) {
+	var ticker;
+
+	json += data;
+
+	try {
+		ticker = JSON.parse(json);
+	} catch (e) {
+		ticker = false;
+	}
+
+	if (ticker) {
+		json = '';
+		last = parseFloat(ticker.ticker.last.value);
+
+		//console.log(last);
+
+		if (!min || last < min) {
+			min = last;
+			sendPush(min);
+		} else {
+			min = min;
+		}
+	}
+}
+
+var prowl = new Prowl('apikey');
+
+function sendPush (value) {
+	var text = '1 btc = $';
+
+	if (timeout) {
+		clearTimeout(timeout);
+		timeout = null;
+	}
+
+	if (!value) {
+		value = min;
+		text = 'Last = $';
+	}
+
+	value = value || min;
+	text += value;
+	text += '\n remain ' + (remain-1);
+
+	//console.log('push', text);
+
+	prowl.push(text, 'mtgox', function (err, remaining) {
+		if (err) {
+			throw err;
+		}
+		remain = remaining;
+	});
+
+	timeout = setTimeout(sendPush, 3600000);
+}
